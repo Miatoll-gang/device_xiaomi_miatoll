d07ed90875050856aedd921adfe9773e0c0060fd
Scissordragonboy <Scissordragonboy1@gmail.com>
2021-12-22 07:31:19 +0000

    sm8150-common: Provide android.frameworks.sensorservice@1.0 on vendor * Used by camera libs.
    Signed-off-by: neobuddy89 <neobuddy89@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -90,6 +90,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hidl.memory.block@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
